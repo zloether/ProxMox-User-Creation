effc62c05d2dcd18a9381794156fb07611261922
Zackary Loether <149006+zloether@users.noreply.github.com>
2020-04-24 13:18:42 -0400

    removing unused code

--- clone_vms.py
+++ clone_vms.py
@@ -12,23 +12,6 @@ from util.proxmox_session_handler import proxmox_session_handler
 
 
 # -----------------------------------------------------------------------------
-# Parse input file
-# -----------------------------------------------------------------------------
-def parse_input_file(input_file, proxmox_session_handler):
-    
-    # read in each line from the file
-    with open(input_file, 'r') as f:
-        content = f.readlines()
-    
-    # iterate through each account and create accounts
-    for line in content:
-        username, password = line.strip().split(',')
-        proxmox_session_handler.create_account(username, password)
-
-
-
-
-# -----------------------------------------------------------------------------
 # Run main
 # -----------------------------------------------------------------------------
 def run_main():
@@ -48,12 +31,5 @@ def run_main():
 # Run interactively
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
-    #if len(sys.argv) < 2:
-    #    print('Provide CSV file as input')
-    #    exit()
-
-    #if not os.path.isfile(sys.argv[1]):
-    #    print('Provide CSV file as input')
-    #    exit()
 
     run_main()
